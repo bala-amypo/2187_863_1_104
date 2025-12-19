79ab45e4c1b311d7c07b44e71353ef9bc1d93331
bala-amypo <bala@amypo.in>
2025-12-19 09:21:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/EmployeeProfileController.java
@@ -0,0 +1,22 @@
+import com.example.demo.model.EmployeeProfile;
+import com.example.demo.service.EmployeeProfileService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/employees")
+@SecurityRequirement(name = "Bearer Authentication")
+public class EmployeeProfileController {
+
+    private final EmployeeProfileService service;
+
+    public EmployeeProfileController(EmployeeProfileService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<EmployeeProfile 
