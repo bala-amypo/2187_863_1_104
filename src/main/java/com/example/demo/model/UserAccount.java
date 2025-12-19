59582ec30c67f299e38e844ecdb771c4afb07f9d
bala-amypo <bala@amypo.in>
2025-12-19 06:48:12 +0000

    User commit

--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -1,3 +1,102 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+
+
+@Entity
+@Table(name="user_accounts")
+public class UserAccount{
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+   
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private Long deviceItemId;
+
+
+    @Column(nullable = false,unique = true)
+    private Boolean isEligible;
+
+    @Column(nullable = false)
+    private String reason;
+
+    @Column(nullable = false , updatable = false)
+    private LocalDateTime checkedAt;
+
+
+    public EligibilityCheckRecord(){
+        
+    }
+    @PrePersist
+    protected void onCreate(){
+    this.checkedAt = LocalDateTime.now();
+}
+    public EligibilityCheckRecord(Long employeeId, Long deviceItemId, String reason,Boolean isEligible){
+        
+    this.employeeId = employeeId;
+    this.deviceItemId = deviceItemId;
+    this.isEligible = isEligible;
+    this.reason = reason;
+    this.checkedAt = LocalDateTime.now();
+    }
+    
+    //Getters and Setters
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+    this.id = id;
+    }
+    public Long getEmployeeId(){
+    return employeeId;
+    }
+    public void setEmployeeId(Long employeeId){
+    this.employeeId = employeeId;
+    }
+    public Long getDeviceItemId(){
+    return deviceItemId;
+    }
+    public void setDeviceItemId(Long deviceItemId){
+    this.deviceItemId = deviceItemId;
+    }
+    
+    public Boolean getIsEligible(){
+        return isEligible;
+    }
+    public void setIsEligible(Boolean isEligible){
+        this.isEligible= isEligible;
+    }
+    public String getReason(){
+        return reason;
+    }
+    public void setReason(String reason){
+        this.reason = reason;
+
+    }
+    public LocalDateTime getCheckedAt(){
+        return checkedAt;
+    }
+    public void setCheckedAt(LocalDateTime checkedAt){
+        this.checkedAt = checkedAt;
+    }
+
+    public boolean isEligible(){
+    return isEligible != null && isEligible;
+    }
+}
+
+
+    
+
+    
+
+
+
+
+
+
