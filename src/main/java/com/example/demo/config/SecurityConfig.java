30da689c9a8f6ef3a3a119ed9aa4e57d66591c9b
bala-amypo <bala@amypo.in>
2025-12-23 08:34:13 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SimplePasswordEncoder passwordEncoder() {
+        return new SimplePasswordEncoder();
+    }
 
     public static class SimplePasswordEncoder {
         public String encode(String password) {
